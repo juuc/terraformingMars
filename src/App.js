a2f926387b20d5d646087b7ad3fc17c2ac5f9d30
juuc <ucheriya@snu.ac.kr>
2023-06-29 07:46:23 +0900

    works

--- src/App.js
+++ src/App.js
@@ -1,22 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
-
+import BubbleChart from './BubbleChart';
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <BubbleChart
+          graph={{
+            zoom: 1.1,
+            offsetX: -0.05,
+            offsetY: -0.01,
+          }}
+          width={500}
+          height={500}
+          padding={0} // optional value, number that set the padding between bubbles
+          showLegend={false} // optional value, pass false to disable the legend.
+          legendPercentage={20} // number that represent the % of with that legend going to use.
+          legendFont={{
+            family: 'Arial',
+            size: 12,
+            color: '#000',
+            weight: 'bold',
+          }}
+          valueFont={{
+            family: 'Arial',
+            size: 12,
+            color: '#fff',
+            weight: 'bold',
+          }}
+          labelFont={{
+            family: 'Arial',
+            size: 16,
+            color: '#fff',
+            weight: 'bold',
+          }}
+          //Custom bubble/legend click functions such as searching using the label, redirecting to other page
+          // bubbleClickFunc={bubbleClick}
+          // legendClickFunc={legendClick}
+          data={[
+            { label: 'CRM', value: 1 },
+            { label: 'API', value: 1 },
+            { label: 'Data', value: 1 },
+            { label: 'Commerce', value: 1 },
+            { label: 'AI', value: 3 },
+            { label: 'Management', value: 5 },
+            { label: 'Testing', value: 6 },
+            { label: 'Mobile', value: 9 },
+            { label: 'Conversion', value: 9 },
+            { label: 'Misc', value: 21 },
+            { label: 'Databases', value: 22 },
+            { label: 'DevOps', value: 22 },
+            { label: 'Javascript', value: 23 },
+            { label: 'Languages / Frameworks', value: 25 },
+            { label: 'Front End', value: 26 },
+            { label: 'Content', value: 26 },
+          ]}
+        />        
       </header>
     </div>
   );
